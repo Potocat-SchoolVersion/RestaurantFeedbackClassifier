df735a24ec732c0eba21868a25d42ca61040493e
Sweating Coder <jasonm-wm21@student.tarc.edu.my>
2025-09-07 02:32:46 +0800

    remove iconn

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
 st.set_page_config(page_title="Graduation QR Scanner", layout="wide")
-menu = st.sidebar.radio("Navigation", ["🏠 Naïve Bayes", "📝 Support Vector Machine", "🎭 BERT"])
+menu = st.sidebar.radio("Navigation", ["Naïve Bayes", "Support Vector Machine", "BERT"])

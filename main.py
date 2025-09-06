89c8b6e58290f31fdd65e9e697c8c7a747c45771
Sweating Coder <jasonm-wm21@student.tarc.edu.my>
2025-09-07 06:04:29 +0800

    change page name

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import streamlit as st
-st.set_page_config(page_title="Graduation QR Scanner", layout="wide")
+st.set_page_config(page_title="Natural Language Processing", layout="wide")
 menu = st.sidebar.radio("Algorithm Model", ["Naïve Bayes", "Support Vector Machine", "BERT"])

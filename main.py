a131110f9a9d436dbbafd9e5f548811d8a634ce3
Sweating Coder <jasonm-wm21@student.tarc.edu.my>
2025-09-07 02:25:47 +0800

    hello

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-
+import streamlit as st
+st.set_page_config(page_title="Graduation QR Scanner", layout="wide")
+menu = st.sidebar.radio("Navigation", ["🏠 Home", "📝 Registration", "🎭 Stage Scanning", "👥 Management", "📊 Reports"])

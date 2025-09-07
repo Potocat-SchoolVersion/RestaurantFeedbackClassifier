8ab901442dcf0d3dfd3a1cbfe955c2d519808296
Sweating Coder <jasonm-wm21@student.tarc.edu.my>
2025-09-07 17:54:28 +0800

    ok

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ if "bert_pipeline" not in st.session_state:
     #bert_results = bert_pipeline(bert_dataset)
 
     st.session_state.bert_pipeline = bert_pipeline
-    st.session_state.y_pred_bert = [bert_label_map[r["label"]] for r in bert_results]
+    #st.session_state.y_pred_bert = [bert_label_map[r["label"]] for r in bert_results]
     with open("./bert_model/y_pred_bert.json", "r") as f:
         st.session_state.y_pred_bert = json.load(f)
 def _rating(prediction):
